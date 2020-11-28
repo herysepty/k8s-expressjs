60001c52132663cb847cddbdf1b53f3b95181a15
herysepty <herysepty6@gmail.com>
2020-11-28 18:25:31 +0700

    Service 2

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+const { default: Axios } = require('axios')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async(req, res) => {
+  
+  try {
+    const getData = await Axios.get('http://10.106.185.70:30001')
+    res.send('Hello World! '+getData.data)
+    
+  } catch (error) {
+    res.status(400).send(error)
+  }
+
 })
 
 app.listen(port, () => {
