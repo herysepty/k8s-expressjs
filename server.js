b9a72ebcdb6b2fadeaef491d27e1823dba766278
herysepty <herysepty6@gmail.com>
2021-10-14 23:08:05 +0700

    Update deployment yml

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const { default: Axios } = require('axios')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3002
 
 app.get('/', async(req, res) => {
   
   try {
-    const getData = await Axios.get('http://10.106.185.70:30001')
-    res.send('Hello World! '+getData.data)
-    
+    // const getData = await Axios.get('http://expressjs-service.default.svc.cluster.local:3000') // access another service
+    // res.send('This is response another service '+getData.data)
+
+    const getData = await Axios.get('http://example-service.default.svc.cluster.local/data/v1/countries') // access another service
+    res.send(getData.data)
   } catch (error) {
     res.status(400).send(error)
   }
